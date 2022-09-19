8d5a31a74b79a8c3686b74b18b74023a2c19f298
Your Name <quentin.garwig@student.vinci.be>
2022-09-19 11:29:37 +0200

    ex1.2 done

---
+++ module1/ex1.2/index.js
@@ -0,0 +1,16 @@
+
+
+
+function addDateTime(message){
+  var date = Date();
+
+  var time= date.toLocaleString();
+  console.log(time + "  : "+ message);
+  alert(time);
+
+
+}
+
+addDateTime("hello tout le monde je suis content de vous voir ajd !");
+
+
